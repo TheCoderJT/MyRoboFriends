a8dc3a70fa568186e4913d73c4a8c025a14afc97
Jordan Turner <gamerslife56@gmail.com>
2021-04-28 20:07:37 -0600

    Robofriends

---
+++ src/components/CardList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ id, name, email, robots }) => {
+  return (
+    <div className="flex flex-wrap justify-center">
+      {robots.map((user, i) => {
+        return (
+          <Card
+            id={robots[i].id}
+            name={robots[i].username}
+            email={robots[i].email}
+            key={robots[i].id}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
